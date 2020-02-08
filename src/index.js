65d4c385db622feefd0a23f9a47ca4badd4d08c6
Joshua Rose <joshua.rose@outlook.com>
2020-02-08 20:15:56 +0000

    Demo dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import { addFingers } from "./fingers";
-
 const message = "I came from javascript";
 
 const messageElement = document.createElement("h1");
-messageElement.textContent = addFingers(message);
+messageElement.textContent = message;
 
 const body = document.getElementsByTagName("body")[0];
 
 body.appendChild(messageElement);
+
+setTimeout(() => {
+    import("./fingers").then(({ addFingers }) => {
+        messageElement.textContent = `${message} - ${addFingers(
+            "dynamically"
+        )}`;
+    });
+}, 3000);
